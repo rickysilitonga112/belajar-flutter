6653814af7fc07bd6e688b58b364e8f92c808af4
rickysilitonga112 <rickysilitonga112@gmail.com>
2021-06-04 10:20:11 +0700

    membuat statefull widge

--- dadu_flutter/lib/main.dart
+++ dadu_flutter/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(MyApp());
@@ -21,18 +23,63 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  const DicePage({Key? key}) : super(key: key);
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left_dice_number = 1;
+  int right_dice_number = 3;
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: <Widget>[
-        Expanded(
-          child: Image.asset('images/dice1.png'),
-        ),
-        Expanded(
-          child: Image.asset('images/dice2.png'),
-        ),
-      ],
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(
+                padding: const EdgeInsets.all(16.0),
+              ),
+              child: Image.asset('images/dice$left_dice_number.png'),
+              onPressed: () {
+                setState(
+                  () {
+                    random(min, max) {
+                      var rn = new Random();
+                      return min + rn.nextInt(max - min);
+                    }
+
+                    left_dice_number = random(1, 6);
+                    print('Left dice number = $left_dice_number');
+                  },
+                );
+              },
+            ),
+          ),
+          Expanded(
+            child: TextButton(
+              style: TextButton.styleFrom(padding: const EdgeInsets.all(16.0)),
+              child: Image.asset('images/dice$right_dice_number.png'),
+              onPressed: () {
+                setState(
+                  () {
+                    random(min, max) {
+                      var rn = new Random();
+                      return min + rn.nextInt(max - min);
+                    }
+
+                    right_dice_number = random(1, 6);
+                    print('Left dice number = $right_dice_number');
+                  },
+                );
+              },
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
